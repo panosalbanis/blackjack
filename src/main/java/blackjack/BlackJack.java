fc6d2302308a0c8b625fe32f56c43e41702d4f6a
panosalbanis <panagiotisal@gmail.com>
2013-10-30 22:51:09 +0000

    Game is playable! A bit messy and buggy but playable

--- src/main/java/blackjack/BlackJack.java
+++ src/main/java/blackjack/BlackJack.java
@@ -1,8 +1,60 @@
 package blackjack;
 
+import blackjack.game.Deck;
+import blackjack.game.Hand;
+
+import java.io.IOException;
+
 public class BlackJack {
 
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
+	private final static int DEALER_MINIMUM = 17;
+	private final static int BLACKJACK = 21;
+	private final static String PLAYER_WINS = "You win!, congratulations.";
+	private final static String DEALER_WINS = "Dealer wins, sorry.";
+
+	public static void main(String[] args) throws IOException {
+		Deck deck = new Deck();
+		Hand dealer = new Hand();
+		Hand player = new Hand();
+
+		while (dealer.value() < DEALER_MINIMUM) {
+			dealer.add(deck.draw());
+		}
+		System.out.println("DEBUG[Dealer stops at: " + dealer.value() + ".]");
+		if (dealer.value() > BLACKJACK) {
+			System.out.println(PLAYER_WINS);
+			return;
+		}
+		if (dealer.value() == BLACKJACK) {
+			System.out.println(DEALER_WINS);
+			return;
+		}
+		player.add(deck.draw());
+		while (true) {
+			System.out.println("You are at " + player.value() + ".");
+			if (player.value() > BLACKJACK) {
+				System.out.println(DEALER_WINS);
+				return;
+			}
+			if (player.value() == BLACKJACK) {
+				System.out.println(PLAYER_WINS);
+				return;
+			}
+			System.out.println("hit? (y/n)");
+			char answer = (char)System.in.read();
+			if (answer == 'y') {
+				player.add(deck.draw());
+			} else if (answer == 'n') {
+				if (player.value() <= dealer.value()) {
+					System.out.println(DEALER_WINS);
+					return;
+				} else {
+					System.out.println(PLAYER_WINS);
+					return;
+				}
+			} else {
+				continue;
+			}
+		}
 	}
 }
